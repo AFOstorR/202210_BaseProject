1039a607c047c0e131fa8fafa1491ce8c5a0dbe2
AFOstorR <af.ostor10@uniandes.edu.co>
2022-04-30 22:04:47 -0500

    Creacion servicio listar-vehiculos.

--- src/app/vehiculo/service/listar-vehiculos.service.ts
+++ src/app/vehiculo/service/listar-vehiculos.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Vehiculo } from '../model/vehiculo';
+import { environment } from '../../../../src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListarVehiculosService {
+  constructor(private http: HttpClient) {}
 
-constructor() { }
-
+  getVehiculos(): Observable<Vehiculo[]>{
+  return this.http.get<Vehiculo[]>(environment.baseUrl);
+  }
 }
