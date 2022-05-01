2d643c8f82764415c159a2de5d262e2f15a52e95
AFOstorR <af.ostor10@uniandes.edu.co>
2022-04-30 22:22:54 -0500

    Creacion componente listar-vehiculos.

--- src/app/vehiculo/component/listar-vehiculos.component.ts
+++ src/app/vehiculo/component/listar-vehiculos.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../model/vehiculo'
+import { ListarVehiculosService } from '../service/listar-vehiculos.service';
 
 @Component({
   selector: 'app-listar-vehiculos',
   templateUrl: './listar-vehiculos.component.html',
   styleUrls: ['./listar-vehiculos.component.css']
 })
-export class ListarVehiculosComponent implements OnInit {
 
-  constructor() { }
+export class ListarVehiculosComponent implements OnInit {
+  vehiculos: Array<Vehiculo> = [];
+
+  constructor(private listarVehiculosService: ListarVehiculosService) { }
 
   ngOnInit() {
+    this.getVehiculos()
+  }
+
+  getVehiculos(){
+    this.listarVehiculosService.getVehiculos().subscribe( (vehiculos) => this.vehiculos = vehiculos);
   }
 
 }
